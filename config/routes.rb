e02b13b07764c49afd6b177fb63ec3d9ea36ed8f
tabigo <info@tabigo.net>
2019-07-26 07:25:05 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+ get 'posts/index'
   resources :todos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+ 
