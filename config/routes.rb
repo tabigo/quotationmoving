2dfdb19e7eb9c73cfb9d01e92c32b3787b5dcf1c
tabigo <info@tabigo.net>
2019-08-17 08:10:06 +0200

    17.8.2019

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
  get 'posts/index'
+  resources :posts
+
+ get 'todos/index'
   resources :todos
-  
+   
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
